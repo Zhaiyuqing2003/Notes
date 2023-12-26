5f1bc73523e36bf6b0f2ab9ca1233d51a1bd61cb
Yuki Hiroshi <48752879+Zhaiyuqing2003@users.noreply.github.com>
2023-12-26 06:09:35 +0000

    from code space.

---
+++ Compiler/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define a (x + 1)
+int x = 2;
+void b() {
+    x = a;
+    printf("%d\n", x);
+}
+void c() {
+    int x = 1;
+    printf("%d\n", a);
+}
+int main() {
+    b();
+    c();
+}
+
